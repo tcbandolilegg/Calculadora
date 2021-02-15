19c5164f06be9b37b3e9be5e1c36513ec32d8e6c
Thereza Bandoli <tcbandolilegg@gmail.com>
2021-02-15 12:55:02 -0300

    fazendo tratamento de numero e operadores

--- scripts/controller.js/CalcController.js
+++ scripts/controller.js/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
   constructor() {
+    this._operation = []
     this._locale = "pt-br"
     this._currentDate
     this._displayCalcEl = document.querySelector("#display")
@@ -28,16 +29,132 @@ class CalcController {
   }
 
 
+  clearAll() {
+
+    this._operation = []
+  }
+  clearEntry() {
+    this._operation.pop()
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1]
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value
+  }
+  isOperator(value) {
+    return (["+", "-", "%", "/"].indexOf(value) > -1)
+  }
+  pushOperation(value) {
+    this._operation.push(value)
+    if (this._operation.length > 3) {
+
+      this.calc()
+
+
+    }
+  }
+
+  calc() {
+
+    let last = this._operation.pop()
+    let result = eval(this._operation.join(""))
+    this._operation = [result, last]
+  }
+
+  setLastNumberToDisplay() {
+
+  }
+  addOperation(value) {
+    console.log("A", isNaN(this.getLastOperation()))
+    console.log(value)
+    console.log(this._operation)
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value)
+      } else if (isNaN(value)) {
+
+        console.log(value)
+      } else {
+        this._operation.push(value)
+
+      }
+    } else {
+      if (this.isOperator(value)) {
+        this._operation.push(value)
+
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString()
+        this.setLastOperation(parseInt(newValue))
+        this.setLastNumberToDisplay()
+      }
+    }
+  }
+  setError() {
+    this.displayCalc = "Error"
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case "ac":
+        this.clearAll()
+        break
+      case "ce":
+        this.clearEntry()
+        break
+      case "porcento":
+        this.addOperation("%")
+
+        break
+      case "divisao":
+        this.addOperation("/")
+
+        break
+      case "multiplicacao":
+        this.addOperation("*")
+
+        break
+      case "subtracao":
+        this.addOperation("-")
+        break
+      case "soma":
+        this.addOperation("+")
+        break
+      case "igual":
+        this.addOperation("=")
+        break
+      case "ponto":
+        this.addOperation(".")
+        break
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.addOperation(parseInt(value))
+        break
+      default:
+        this.setError()
+        break
+    }
+
+  }
+
   initButtonsEvents() {
     let buttons = document.querySelectorAll('#buttons > g , #parts > g');
 
     buttons.forEach((btn, index) => {
-      this.addEventListenerAll(btn, 'click drag mouseover', e => {
+      this.addEventListenerAll(btn, 'click drag', e => {
 
-        // let textBtn = btn.className.baseVal.replace('btn-', '');
-        console.log(btn.className.baseVal.replace('btn-', ''))
-
-        // this.execBtn(textBtn);
+        let textBtn = btn.className.baseVal.replace('btn-', '');
+        this.execBtn(textBtn);
 
       });
 
@@ -45,8 +162,7 @@ class CalcController {
         btn.style.cursor = "pointer"
       })
 
-      // this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => { //troca o modelo seta do mouse para a maozinha
-      //   btn.style.cursor = 'pointer';
+
     });
   }
 
